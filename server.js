5aaa774ed38e05fa9ec328e5b2b0e3f9b511de45
Chris Benson <ChrisBenson87@gmail.com>
2022-05-04 10:56:20 +0100

    backend good

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./loginAPI/user/routes");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
